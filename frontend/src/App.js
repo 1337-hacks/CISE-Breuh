d0746dea9aa64029a03c321defbc442e39d7df35
cieloren <roquid.1217@outlook.com>
2021-09-21 23:31:59 +1200

    Co-authored-by: 1337-hacks <1337-hacks@users.noreply.github.com> Co-authored-by: ChilaquilesEksDee <ChilaquilesEksDee@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import PageLayout from './Components/Layout';
 
 // Pages
+import HomePage from './Components/Pages/Home';
 import DisplayEvidencePage from './Components/Pages/DisplayEvidence';
 
 class App extends Component {
@@ -13,17 +16,26 @@ class App extends Component {
   }
 
 
-  makeIncrementer = amount => () =>
-    this.setState(prevState => ({
-      count: prevState.count + amount,
-    }));
+  // makeIncrementer = amount => () =>
+  //   this.setState(prevState => ({
+  //     count: prevState.count + amount,
+  //   }));
 
-  increment = this.makeIncrementer(1);
+  // increment = this.makeIncrementer(1);
 
   render() {
     return (
-    <>
-      <div>
+      <>
+        <Router>
+          <PageLayout>
+            <Switch>
+              <Route path="/" component={HomePage} exact />
+              <Route path="/displayEvidence" component={DisplayEvidencePage} />
+            </Switch>;
+          </PageLayout>
+        </Router>
+
+        {/* <div>
         <p>Count: {this.state.count}</p>
         <button className="increment" onClick={this.increment}>Increment count</button>
       </div>
@@ -31,9 +43,9 @@ class App extends Component {
       <Router>
         <Route path='/disEvidence' component={DisplayEvidencePage} />
       </Router>
-      
+      */}
       </>
-    )
+    );
   }
 }
 
