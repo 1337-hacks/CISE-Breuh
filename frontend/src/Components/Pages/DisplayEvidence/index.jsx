98b441289c3e32e27d37006c65136f4de69d65f5
1337-hacks <elijahnucum.exe@gmail.com>
2021-09-27 21:21:37 +1300

    Commit 014 - Backend + Frontend Update

--- frontend/src/Components/Pages/DisplayEvidence/index.jsx
+++ frontend/src/Components/Pages/DisplayEvidence/index.jsx
@@ -1,3 +1,6 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import { Link } from "react-router-dom";
 import { 
   PageWrapper, 
@@ -13,10 +16,37 @@ import tablecolumns from "../../Evidence-Table/tablecolumns";
 // import Dropdown from "../../Evidence-Table/dropdown";
 // import Styles from "../../Evidence-Table/tablestyle";
 
-const DisplayEvidencePage = () => {
 
-    /* Logic goes here eg. pull data from DB */
-    
+
+class DisplayEvidencePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: {}
+    };
+  }
+
+  /* Logic goes here eg. pull data from DB */
+
+  // componentDidMount() {
+  //   // console.log("Print id: " + this.props.match.params.id);
+  //   axios
+  //     .get('/routes/articles/'+this.props.match.params.id)
+  //     .then(res => {
+  //       // console.log("Print-showBookDetails-API-response: " + res.data);
+  //       this.setState({
+  //         articles: res.data
+  //       })
+  //     })
+  //     .catch(err => {
+  //       console.log("Error from ShowBookDetails");
+  //     })
+  // };
+
+  render() {
+
+    //const articles = this.state.articles;
+
     return (
       <PageWrapper>
         {/* Container for Page Background */}
@@ -28,10 +58,7 @@ const DisplayEvidencePage = () => {
                 <h2>Select SE Practice to get evidence for the claimed benefits</h2>
                 {/* <Dropdown/>
                 <Styles> */}
-                    <Table
-                    data={articles}
-                    columns={tablecolumns}
-                    />
+                    <Table data={articles} columns={tablecolumns}/>
                 {/* </Styles> */}
             </div>
               <Link to="/">
@@ -42,6 +69,8 @@ const DisplayEvidencePage = () => {
         </PageContainer>
       </PageWrapper>
     );
-};
+  }
+  
+}
 
 export default DisplayEvidencePage
