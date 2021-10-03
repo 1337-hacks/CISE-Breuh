ef4a28042753b5d4684e7ce71a043ca10c028977
1337-hacks <elijahnucum.exe@gmail.com>
2021-10-03 17:17:00 +1300

    Commit 019 - Feature Complete

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,27 @@
 // app.js
 
 const express = require("express");
-const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 7000;
+const app = express();
+const path = require('path');
+require("./db/conn");
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/articles"));
 
-// get driver connection
-const dbo = require("./db/conn");
- 
+// API
+
+const articles = require("./routes/articles");
+app.use("/routes/articles", articles);
+
+// app.use(express.static(path.join(__dirname, '../build')))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build'))
+// }) 
+
+// Start Server
+
+const port = process.env.PORT || 7000;
 app.listen(port, () => {
-
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
+    console.log(`Server started on port ${port}`);
 });
